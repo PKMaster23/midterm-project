3fd5acfda4b6dbed5a6425ca55e64b4c09533c90
Luke Freeman <mlfreeman2004@gmail.com>
2024-03-19 13:44:52 -0500

    Created the code for the Card class.

---
+++ lib/five_card_draw_poker.rb
@@ -0,0 +1,17 @@
+
+class Card
+  attr_reader :suit, :value
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  def get_suit
+    @suit
+  end
+
+  def get_value
+    @value
+  end
+end
